aff9f612007bc9d9f26b70a8c70ae0d2fe682a38
levidiego <levidiego@gmail.com>
2020-12-29 22:15:14 -0300

    pagina inicial
    Primeira pagina feita

---
+++ index.php
@@ -0,0 +1,177 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<?php require_once ('head.php'); ?>
+<style type="text/css">
+<!--
+body,td,th {
+	font-family: tahoma, verdana;
+}
+a:link {
+	color: #333333;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+	color: #333333;
+}
+a:hover {
+	text-decoration: none;
+	color: #333333;
+}
+a:active {
+	text-decoration: none;
+	color: #333333;
+}
+.botao{
+	border:#006699 solid 1px;
+}
+.style6 {font-size: 12px}
+.style7 {
+	text-align: left;
+}
+-->
+</style>
+<body >
+ <?php require_once ('header.php');?>
+
+        <section class="page-title center bg-overlay-black-50 parallax" style="background: url(imagem/plenitude3.jpg);">  <!-- class="page-section-ptb theme-bg" -->
+            <div class="container">
+                <div class="row ">
+                    <div class="col-12">
+                        <div class="section-title text-center"></br></br></br><br></br><br>
+                           <!-- <center><img src="../imagens/logoPlenus.png" height="50px"/></center><br> --> <!-- class="title-white-about" -->
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 xs-mb-30">
+                        <div class="feature-text text-center">
+                            <div class="feature-icon">
+                                <span aria-hidden="true" class="ti-layers-alt text-white"></span>
+                            </div>
+                            <h4 >Imagens</h4> <!-- class="text-white pt-20" -->
+                            <p >Textos e mais textos </p> <!-- class="citacaoabout-white" -->
+                        </div>
+                    </div>
+					                    <div class="col-lg-4 col-md-4 col-sm-4 xs-mb-30">
+                        <div class="feature-text text-center">
+                            <div class="feature-icon">
+                                <span aria-hidden="true" class="ti-layers-alt text-white"></span>
+                            </div>
+                            <h4 >Imagens</h4> <!-- class="text-white pt-20" -->
+                            <p >Textos e mais textos </p> <!-- class="citacaoabout-white" -->
+                        </div>
+                    </div>
+					                    <div class="col-lg-4 col-md-4 col-sm-4 xs-mb-30">
+                        <div class="feature-text text-center">
+                            <div class="feature-icon">
+                                <span aria-hidden="true" class="ti-layers-alt text-white"></span>
+                            </div>
+                            <h4 >Imagens</h4> <!-- class="text-white pt-20" -->
+                            <p >Textos e mais textos </p> <!-- class="citacaoabout-white" -->
+                        </div>
+                    </div>
+					                    <div class="col-lg-4 col-md-4 col-sm-4 xs-mb-30">
+                        <div class="feature-text text-center">
+                            <div class="feature-icon">
+                                <span aria-hidden="true" class="ti-layers-alt text-white"></span>
+                            </div>
+                            <h4 >Imagens</h4> <!-- class="text-white pt-20" -->
+                            <p >Textos e mais textos </p> <!-- class="citacaoabout-white" -->
+                        </div>
+                    </div>
+					                    <div class="col-lg-4 col-md-4 col-sm-4 xs-mb-30">
+                        <div class="feature-text text-center">
+                            <div class="feature-icon">
+                                <span aria-hidden="true" class="ti-layers-alt text-white"></span>
+                            </div>
+                            <h4 >Imagens</h4> <!-- class="text-white pt-20" -->
+                            <p >Textos e mais textos </p> <!-- class="citacaoabout-white" -->
+                        </div>
+                    </div>
+					                    <div class="col-lg-4 col-md-4 col-sm-4 xs-mb-30">
+                        <div class="feature-text text-center">
+                            <div class="feature-icon">
+                                <span aria-hidden="true" class="ti-layers-alt text-white"></span>
+                            </div>
+                            <h4 >Imagens</h4> <!-- class="text-white pt-20" -->
+                            <p >Textos e mais textos </p> <!-- class="citacaoabout-white" -->
+                        </div>
+                    </div>
+					                    <div class="col-lg-4 col-md-4 col-sm-4 xs-mb-30">
+                        <div class="feature-text text-center">
+                            <div class="feature-icon">
+                                <span aria-hidden="true" class="ti-layers-alt text-white"></span>
+                            </div>
+                            <h4 >Imagens</h4> <!-- class="text-white pt-20" -->
+                            <p >Textos e mais textos </p> <!-- class="citacaoabout-white" -->
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 xs-mb-30">
+                        <div class="feature-text text-center">
+                            <div class="feature-icon">
+                                <span aria-hidden="true" class="ti-image text-white"></span>
+                            </div>
+                            <h4 >Imagens</h4> <!-- class="text-white pt-20" -->
+                            <p >Textos e mais textos </p> <!-- class="citacaoabout-white" -->
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4">
+                        <div class="feature-text text-center">
+                            <div class="feature-icon">
+                                <span aria-hidden="true" class="ti-heart text-white"></span>
+                            </div>
+                            <h4 >Imagens</h4> <!-- class="text-white pt-20" -->
+                            <p >Textos e mais textos </p>  <!-- class="citacaoabout-white acessibilidade"  -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+         
+            <div >
+				</br></br></br>
+				</br></br></br>
+				</br></br>
+		
+            </div>
+        </section>
+ 
+ <?php include 'footer.php'; ?>	 
+
+    <!-- jquery -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+
+    <!-- plugins-jquery -->
+    <script src="js/plugins-jquery.js"></script>
+    <script src="js/fonte.js"></script>
+
+    <!-- plugin_path -->
+    <script>
+        var plugin_path = 'js/';
+    </script>
+
+    <!-- REVOLUTION JS FILES -->
+    <script src="revolution/js/jquery.themepunch.tools.min.js"></script>
+    <script src="revolution/js/jquery.themepunch.revolution.min.js"></script>
+
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+    <script src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
+    <script src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+    <script src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
+    <script src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+    <script src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script src="revolution/js/extensions/revolution.extension.video.min.js"></script>
+    <!-- revolution custom -->
+    <script src="revolution/js/revolution-custom.js"></script>
+
+    <!-- custom -->
+    <script src="js/custom.js"></script>
+
+    
+    <!-- smooth-scroll -->
+    <script src="js/app.js"></script> 
+</body>
+</html>
+
